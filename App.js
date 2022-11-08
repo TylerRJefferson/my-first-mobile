507bc70f7c13b3afa1185ef66eb1330ab772509e
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-08 15:17:28 -0500

    played with props, state, styling in Native

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Counter from './components/Counter';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome firstName={'Tyler'}/>
+      <Text style={styles.bigText}>Hello World! 😎👻👾</Text>
+      <Counter/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  bigText: {
+    fontSize: '120vw',
+    fontWeight: "bold",
+    color: 'white',
+    textAlign: 'center'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aqua',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Button, Text, StyleSheet } from 'react-native';
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+  return(
+    <>
+      <Text style={styles.medText}>Counter: {count}</Text>
+      <Button 
+        title={'Count +1'}
+        onPress = {() => setCount(count + 1)}
+        color="#cc0044" 
+        accessibilityLabel='Count +1'/>
+      <Button 
+        title={'Reset Counter'}
+        onPress = {() => setCount(0)}
+        color="#cc0044"
+        accessibilityLabel='Reset Counter'/>
+      <Button
+        title={'Count -1'}
+        onPress = {() => setCount(count - 1)}
+        color="#cc0044"
+        accessibilityLabel='Count -1' />
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  medText: {
+    fontSize: 24,
+    margin: 16
+  }
+})
